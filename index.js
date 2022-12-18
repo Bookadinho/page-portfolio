5d2969b0b9e981623a843af5eb4d64aa4fed5316
anesteralv <58110736+anesteralv@users.noreply.github.com>
2022-12-17 21:27:21 -0300

    teste

---
+++ index.js
@@ -0,0 +1,195 @@
+function showElement1() {
+    // get the clock
+    var myDescription = document.getElementById('point');
+    
+
+    // get the current value of the clock's display property
+    var displaySetting = myDescription.style.display;
+
+    // also get the clock button, so we can change what it says
+    var descriptionButton = document.getElementById('description');
+
+    // now toggle the clock and the button text, depending on current state
+ if (displaySetting == 'none') {
+         myDescription.style.display = 'block';
+         // change button text
+         descriptionButton.innerHTML = 'Esconde descrição';
+        
+     }
+ else {
+         // clock is visible. hide it
+         myDescription.style.display = 'none';
+        // change button text
+        descriptionButton.innerHTML = 'Mostra descrição';
+        // clock is hidden. show it
+    
+}
+}
+
+ function showElement2() {
+         // get the clock
+         var myDescription = document.getElementById('point1');
+    
+
+         // get the current value of the clock's display property
+         var displaySetting = myDescription.style.display;
+
+         // also get the clock button, so we can change what it says
+         var descriptionButton = document.getElementById('description1');
+
+         // now toggle the clock and the button text, depending on current state
+         if (displaySetting == 'none') {
+             myDescription.style.display = 'block';
+             // change button text
+             descriptionButton.innerHTML = 'Esconde descrição';
+        
+          }
+         else {
+             // clock is visible. hide it
+             myDescription.style.display = 'none';
+             // change button text
+             descriptionButton.innerHTML = 'Mostra descrição';
+             // clock is hidden. show it
+    
+}
+}
+
+         function showElement3() {
+             // get the clock
+             var myDescription = document.getElementById('point2');
+    
+
+             // get the current value of the clock's display property
+             var displaySetting = myDescription.style.display;
+
+             // also get the clock button, so we can change what it says
+             var descriptionButton = document.getElementById('description2');
+
+              // now toggle the clock and the button text, depending on current state
+             if (displaySetting == 'none') {
+                 myDescription.style.display = 'block';
+                 // change button text
+                 descriptionButton.innerHTML = 'Esconde descrição';
+        
+     }
+              else {
+                 // clock is visible. hide it
+                 myDescription.style.display = 'none';
+                 // change button text
+                 descriptionButton.innerHTML = 'Mostra descrição';
+                 // clock is hidden. show it
+    
+}
+}
+
+function showElement4() {
+    // get the clock
+    var myDescription = document.getElementById('point3');
+    
+
+    // get the current value of the clock's display property
+    var displaySetting = myDescription.style.display;
+
+    // also get the clock button, so we can change what it says
+    var descriptionButton = document.getElementById('description3');
+
+    // now toggle the clock and the button text, depending on current state
+ if (displaySetting == 'none') {
+         myDescription.style.display = 'block';
+         // change button text
+         descriptionButton.innerHTML = 'Esconde descrição';
+        
+     }
+ else {
+         // clock is visible. hide it
+         myDescription.style.display = 'none';
+        // change button text
+        descriptionButton.innerHTML = 'Mostra descrição';
+        // clock is hidden. show it
+    
+}
+}
+
+function showElement5() {
+    // get the clock
+    var myDescription = document.getElementById('point4');
+    
+
+    // get the current value of the clock's display property
+    var displaySetting = myDescription.style.display;
+
+    // also get the clock button, so we can change what it says
+    var descriptionButton = document.getElementById('description4');
+
+    // now toggle the clock and the button text, depending on current state
+ if (displaySetting == 'none') {
+         myDescription.style.display = 'block';
+         // change button text
+         descriptionButton.innerHTML = 'Esconde descrição';
+        
+     }
+ else {
+         // clock is visible. hide it
+         myDescription.style.display = 'none';
+        // change button text
+        descriptionButton.innerHTML = 'Mostra descrição';
+        // clock is hidden. show it
+    
+}
+}
+
+function showElement1() {
+    // get the clock
+    var myDescription = document.getElementById('point');
+    
+
+    // get the current value of the clock's display property
+    var displaySetting = myDescription.style.display;
+
+    // also get the clock button, so we can change what it says
+    var descriptionButton = document.getElementById('description');
+
+    // now toggle the clock and the button text, depending on current state
+ if (displaySetting == 'none') {
+         myDescription.style.display = 'block';
+         // change button text
+         descriptionButton.innerHTML = 'Esconde descrição';
+        
+     }
+ else {
+         // clock is visible. hide it
+         myDescription.style.display = 'none';
+        // change button text
+        descriptionButton.innerHTML = 'Mostra descrição';
+        // clock is hidden. show it
+    
+}
+}
+
+function showElement1() {
+    // get the clock
+    var myDescription = document.getElementById('point');
+    
+
+    // get the current value of the clock's display property
+    var displaySetting = myDescription.style.display;
+
+    // also get the clock button, so we can change what it says
+    var descriptionButton = document.getElementById('description');
+
+    // now toggle the clock and the button text, depending on current state
+ if (displaySetting == 'none') {
+         myDescription.style.display = 'block';
+         // change button text
+         descriptionButton.innerHTML = 'Esconde descrição';
+        
+     }
+ else {
+         // clock is visible. hide it
+         myDescription.style.display = 'none';
+        // change button text
+        descriptionButton.innerHTML = 'Mostra descrição';
+        // clock is hidden. show it
+    
+}
+}
